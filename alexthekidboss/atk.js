ff5369dc56800d1d7a042e38ecc39a503fba4e0e
stephen9003 <stephen@stephenthomas.info>
2024-06-14 10:23:14 +0100

    add reset functionality needing some class refacts

--- alexthekidboss/atk.js
+++ alexthekidboss/atk.js
@@ -32,15 +32,15 @@ function userSelection(event) {
     switch(event.key.toUpperCase()){
         case 'R':
             playerChoice.style.backgroundPosition = '0px 0px';
-            playerFinalSelection = 'Rock';
+            playerFinalSelection = 'R';
             break;
         case 'S':
             playerChoice.style.backgroundPosition = '-101px 0px';
-            playerFinalSelection = 'Scissors';
+            playerFinalSelection = 'S';
             break;
         case 'P':
             playerChoice.style.backgroundPosition = '-202px 0px';
-            playerFinalSelection = 'Paper';
+            playerFinalSelection = 'P';
             break;
     }
 }
@@ -52,9 +52,9 @@ function result(){
     document.removeEventListener('keydown', userSelection);
 
     //stop annimations
-    document.getElementById('cpuChoice').style.animation = 'none';
-    document.getElementById('cpu').style.animation = 'none';
-    document.getElementById('human').style.animation = 'none';
+    document.getElementById('cpuChoice').classList.remove('cpuChoiceAnim');
+    document.getElementById('cpu').classList.remove('cpuIdleAnim');
+    document.getElementById('human').classList.remove('playerIdleAnim');
 
     // decide and show the CPU's choice
     let cpuFinalSelection = Math.floor(Math.random() * 3) + 1;
@@ -72,7 +72,7 @@ function result(){
             document.getElementById('cpuChoice').style.backgroundPosition = '-203px 0';
             break;
     }
-    console.log(cpuFinalSelection);
+
     // determine the winner
     let finalDisplay = 'WIN!!!'
     if (playerFinalSelection === cpuFinalSelection) {
@@ -84,11 +84,32 @@ function result(){
     } else if (playerFinalSelection === 'P' && cpuFinalSelection === 'S') {
         finalDisplay = 'lose'
     }
-    document.getElementById('result').style.display = 'block';
-    document.getElementById('result').innerHTML = '<p>You ' + finalDisplay + '</p><button onclick="reset()">Try again?</button>'; 
+    const collection = document.getElementsByClassName('choice');
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].classList.add('wobble');
+    }
+    setTimeout(() => {
+        document.getElementById('result').style.display = 'block';
+        document.getElementById('result').innerHTML = '<p>You ' + finalDisplay + '</p><button onclick="reset()">Try again?</button>'; 
+    }, 1300);
 }
 
 function reset() {
-    location.reload();
+    //reset variables
+    playerFinalSelection = 'R'
+
+    //reset classes
+    document.getElementById('cpuChoice').classList.add('cpuChoiceAnim');
+    document.getElementById('cpu').classList.add('cpuIdleAnim');
+    document.getElementById('human').classList.add('playerIdleAnim');
+    const collection = document.getElementsByClassName('choice');
+    document.getElementById('result').style.display = 'none';
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].classList.remove('wobble');
+    }
+    //readd event listener
+    document.addEventListener('keydown', userSelection);
+
+    //start the game
     start();
 }
